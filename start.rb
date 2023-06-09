f07983e20c1b08f936c06e1d76a4680e2095e732
HttpRafa <rafael@kienitz.link>
2023-06-09 16:00:14 +0200

    Cleaned up the code and fixed some typos

--- start.rb
+++ start.rb
@@ -1,11 +1,11 @@
 require 'net/http'
 require 'json'
 
-$serverUrls = { 
+$server_urls = {
 #    custom: "https://kienitz.link/host/minecraft/installer/", 
     paperMC: "https://api.papermc.io/v2/projects/" 
 }
-$softwareTypes = {
+$software_types = {
 #    raper: "custom", 
     paper: "paperMC", 
     velocity: "paperMC" 
@@ -37,32 +37,32 @@ end
 
 class ServerSettings 
 
-    attr_reader :javaBin, :restartTime, :jvmArgs, :serverArgs
+    attr_reader :java_bin, :restart_time, :jvm_args, :server_args
 
-    def initialize(javaBin, restartTime, jvmArgs, serverArgs)
-        @javaBin = javaBin
-        @restartTime = restartTime
-        @jvmArgs = jvmArgs
-        @serverArgs = serverArgs
+    def initialize(java_bin, restart_time, jvm_args, server_args)
+        @java_bin = java_bin
+        @restart_time = restart_time
+        @jvm_args = jvm_args
+        @server_args = server_args
     end
 
     def to_json(*args)
         {
-            javaBin: @javaBin,
-            restartTime: @restartTime,
-            jvmArgs: @jvmArgs,
-            serverArgs: @serverArgs
+          java_bin: @java_bin,
+          restart_time: @restart_time,
+          jvm_args: @jvm_args,
+          server_args: @server_args
         }.to_json(*args)
     end
 
 end
 
-def request_number()
+def request_number
     found = false
-    while !found do
+    until found do
         puts "[INPUT] Please enter a number"
         value = gets.chomp
-        if value.to_i.to_s == value then
+        if value.to_i.to_s == value
             return value.to_i
         end
         puts "[INPUT] Failed to parse number. Please try again"
@@ -70,20 +70,18 @@ def request_number()
 end
 
 def request_answer(values)
-    found = false
-    while !found do
+    until $i > 10000 do
         $i = 0
         values.each {
-            | value |
-                puts "#$i | #{value}"
-                $i += 1
+          |item|
+            puts "#{$i} | #{item}"
+            $i += 1
         }
         puts "[INPUT] Please enter a number from 0 - #{values.size - 1}"
         value = gets.chomp
-        if value.to_i.to_s == value then
+        if value.to_i.to_s == value
             value = value.to_i
-            if value < values.size then
-                found = true
+            if value < values.size
                 return values[value]
             end
         end
@@ -91,10 +89,10 @@ def request_answer(values)
     end
 end
 
-def request_software()
-    software = request_answer $softwareTypes.keys
+def request_software
+    software = request_answer $software_types.keys
     puts "-------------- [ #{software} ] --------------"
-    return software
+    software
 end
 
 def request_software_version(provider, software)
@@ -102,31 +100,33 @@ def request_software_version(provider, software)
     puts "[NETWORK] Fetching versions..."
     case provider
     when "custom"
-        url = URI.parse("#{$serverUrls[provider.to_sym]}#{software}/versions.json")
+        url = URI.parse("#{$server_urls[provider.to_sym]}#{software}/versions.json")
         response = Net::HTTP.get_response(url)
-        if response.is_a?(Net::HTTPSuccess) then
+        if response.is_a?(Net::HTTPSuccess)
             json_data = JSON.parse(response.body)
-            for version in json_data do
+            json_data.each { |version|
                 versions.append(version)
-            end
+            }
         else
             puts "Error: #{response.code} - #{response.message}"
         end
     when "paperMC"
-        url = URI.parse("#{$serverUrls[provider.to_sym]}#{software}")
+        url = URI.parse("#{$server_urls[provider.to_sym]}#{software}")
         response = Net::HTTP.get_response(url)
-        if response.is_a?(Net::HTTPSuccess) then
+        if response.is_a?(Net::HTTPSuccess)
             json_data = JSON.parse(response.body)
-            for version in json_data["versions"] do
+            json_data["versions"].each { |version|
                 versions.append(version)
-            end
+            }
         else
             puts "Error: #{response.code} - #{response.message}"
         end
+    else
+        puts "Unknown provider: #{provider}"
     end
     version = request_answer versions
     puts "-------------- [ #{version} ] --------------"
-    return version
+    version
 end
 
 def complete_version(provider, software, version)
@@ -134,57 +134,59 @@ def complete_version(provider, software, version)
     puts "[NETWORK] Fetching builds..."
     case provider
     when "custom"
-        url = URI.parse("#{$serverUrls[provider.to_sym]}#{software}/#{version}/builds.json")
+        url = URI.parse("#{$server_urls[provider.to_sym]}#{software}/#{version}/builds.json")
         response = Net::HTTP.get_response(url)
-        if response.is_a?(Net::HTTPSuccess) then
+        if response.is_a?(Net::HTTPSuccess)
             json_data = JSON.parse(response.body)
-            for build in json_data do
+            json_data.each { |build|
                 builds[build["build"]] = build["file"]
-            end
+            }
         else
             puts "Error: #{response.code} - #{response.message}"
         end
     when "paperMC"
-        url = URI.parse("#{$serverUrls[provider.to_sym]}#{software}/versions/#{version}/builds")
+        url = URI.parse("#{$server_urls[provider.to_sym]}#{software}/versions/#{version}/builds")
         response = Net::HTTP.get_response(url)
-        if response.is_a?(Net::HTTPSuccess) then
+        if response.is_a?(Net::HTTPSuccess)
             json_data = JSON.parse(response.body)
-            for build in json_data["builds"] do
+            json_data["builds"].each { |build|
                 builds[build["build"]] = build["downloads"]["application"]["name"]
-            end
+            }
         else
             puts "Error: #{response.code} - #{response.message}"
         end
+    else
+        puts "Unknown provider: #{version.provider}"
     end
-    maxBuild = builds.keys[0]
-    maxFile = builds.values[0]
+    max_build = builds.keys[0]
+    max_file = builds.values[0]
     builds.each {
-        | key, value | 
-            if key > maxBuild then
-                maxBuild = key
-                maxFile = builds[key]
+        | key, _|
+            if key > max_build
+                max_build = key
+                max_file = builds[key]
             end
     }
-    puts "[VERSION] Latest build is #{maxBuild}"
-    return Version.new(provider, software, version, maxBuild, maxFile)
+    puts "[VERSION] Latest build is #{max_build}"
+    Version.new(provider, software, version, max_build, max_file)
 end
 
-def check_eula()
-    if File.exists?("installation.json") && File.read("eula.txt").include?("eula=true") then
+def check_eula
+    if File.exists?("installation.json") && File.read("eula.txt").include?("eula=true")
         puts "[EULA] Accepted"
     else
         puts "-------------- [ eula ] --------------"
         puts "By accepting this EULA, you acknowledge that failure to comply with the terms may result in the termination of your access to Minecraft."
-        answer = request_answer ["yes", "no"]
-        if answer == "yes" then
+        answer = request_answer %w[yes no]
+        if answer == "yes"
             File.write("eula.txt", "eula=true")
             puts "[EULA] Accepted"
         end
     end
 end
 
-def load_settings()
-    if !File.exists?("settings.json") then
+def load_settings
+    if !File.exists?("settings.json")
         puts "[SCRIPT] No settings found"
         puts "[SETTINGS] Please enter the amount of RAM(in MB) the server should have"
         ram = request_number # Not further checks if the user enters -1 or something very big how cares
@@ -194,31 +196,31 @@ def load_settings()
         File.open("settings.json", "w") do |file|
             file.write(json_data)
         end
-        return settings
+        settings
     else
         puts "[SCRIPT] Loading current installation"
         json_data = JSON.parse(File.read("settings.json"))
-        return ServerSettings.new(json_data["javaBin"], json_data["restartTime"], json_data["jvmArgs"], json_data["serverArgs"])
+        ServerSettings.new(json_data["java_bin"], json_data["restart_time"], json_data["jvm_args"], json_data["server_args"])
     end
 end
 
-def check_installation()
-    if !File.exists?("installation.json") then
+def check_installation
+    if !File.exists?("installation.json")
         puts "[SCRIPT] Script is running for the first time"
-        return nil
+        nil
     else
         puts "[SCRIPT] Loading current installation"
         json_data = JSON.parse(File.read("installation.json"))
-        return Version.new(json_data["provider"], json_data["software"], json_data["version"], json_data["build"], json_data["file"])
+        Version.new(json_data["provider"], json_data["software"], json_data["version"], json_data["build"], json_data["file"])
     end
 end
 
 def check_for_updates(version)
-    lastestVersion = complete_version version.provider, version.software, version.version
-    if lastestVersion.build > version.build then
-        return lastestVersion
+    latest_version = complete_version version.provider, version.software, version.version
+    if latest_version.build > version.build
+        latest_version
     else
-        return nil
+        nil
     end
 end
 
@@ -233,37 +235,39 @@ def download_version(version)
     puts "[VERSION] Downloading... Depending on your internet connection, this may take some time"
     case version.provider
     when "custom"
-        url = URI.parse("#{$serverUrls[version.provider.to_sym]}#{version.software}/#{version.version}/#{version.build}/#{version.file}")
+        url = URI.parse("#{$server_urls[version.provider.to_sym]}#{version.software}/#{version.version}/#{version.build}/#{version.file}")
         File.write(version.file, Net::HTTP.get(url))
     when "paperMC"
-        url = URI.parse("#{$serverUrls[version.provider.to_sym]}#{version.software}/versions/#{version.version}/builds/#{version.build}/downloads/#{version.file}")
+        url = URI.parse("#{$server_urls[version.provider.to_sym]}#{version.software}/versions/#{version.version}/builds/#{version.build}/downloads/#{version.file}")
         File.write(version.file, Net::HTTP.get(url))
+    else
+        puts "Unknown provider: #{version.provider}"
     end
 end
 
-def update(oldVersion, newVersion)
-    puts "[VERSION] Update found. Current version: #{oldVersion.build} | Lastest version: #{newVersion.build}"
-    File.delete(oldVersion.file)
+def update(old_version, new_version)
+    puts "[VERSION] Update found. Current version: #{old_version.build} | Latest version: #{new_version.build}"
+    File.delete(old_version.file)
 
     # Check eula
     check_eula
 
     # Install version
-    download_version newVersion
+    download_version new_version
 
     # Write version information
-    write_version newVersion
+    write_version new_version
 end
 
 # Main
-serverSettings = load_settings
+server_settings = load_settings
 
 installation = check_installation
-if !installation.nil? then
-    lastestVersion = check_for_updates installation
-    if !lastestVersion.nil? then
-        update installation, lastestVersion
-        installation = lastestVersion
+if !installation.nil?
+    latest_version = check_for_updates installation
+    if !latest_version.nil?
+        update installation, latest_version
+        installation = latest_version
     else
         puts "[VERSION] Up to date"
 
@@ -273,7 +277,7 @@ if !installation.nil? then
 else
     puts "-------------- [ software ] --------------"
     software = request_software
-    provider = $softwareTypes[software]
+    provider = $software_types[software]
     version = request_software_version provider, software
     installation = complete_version provider, software, version
 
@@ -292,22 +296,22 @@ running = true
 while running do
     
     # Start the server
-    command = "#{serverSettings.javaBin} #{serverSettings.jvmArgs} -jar #{installation.file} #{serverSettings.serverArgs}"
+    command = "#{server_settings.java_bin} #{server_settings.jvm_args} -jar #{installation.file} #{server_settings.server_args}"
     puts "[EXECUTE] #{command}"
     system(command)
     puts "-------------- [ stopped ] --------------"
 
-    # Wait for restartTime seconds
-    serverSettings.restartTime.downto(1) do |x|
+    # Wait for restart_time seconds
+    server_settings.restart_time.downto(1) do |x|
         puts "[RESTART] Waiting for #{x} seconds"
         sleep(1)
     end
 
     # Check for updates
-    lastestVersion = check_for_updates installation
-    if !lastestVersion.nil? then
-        update installation, lastestVersion
-        installation = lastestVersion
+    latest_version = check_for_updates installation
+    if !latest_version.nil?
+        update installation, latest_version
+        installation = latest_version
     else
         puts "[VERSION] Up to date"
     end
